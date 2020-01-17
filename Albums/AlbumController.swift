932cfb450f5f160a844ea3fe183c814c46ffc620
Craig Swanson <cswanson02@me.com>
2020-01-16 15:46:40 -1000

    Successfully ran the test decoding and encoding methods together.

--- Albums/AlbumController.swift
+++ Albums/AlbumController.swift
@@ -9,20 +9,43 @@
 import Foundation
 
 class AlbumController {
-    
+            var decodedAlbum: Album?
+    var encodedAlbum: Data?
     func testDecodingExampleAlbum() {
-  
-            if let url = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json") {
-            
-        do {
-            let downloadedData = try Data(contentsOf: url)
-            let decoder = JSONDecoder()
-            let decodedAlbum = try! decoder.decode(Album.self, from: downloadedData)
-        } catch {
-            print("Error in decoding the json: \(error)")
-        }
         
+        if let url = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json") {
+            
+            do {
+                let downloadedData = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decodedAlbum = try! decoder.decode(Album.self, from: downloadedData)
+                print(decodedAlbum)
+            } catch {
+                print("Error in decoding the json: \(error)")
             }
+            
+        }
+    }
     
+    func testEncodingExampleAlbum() {
+
+        if let url = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json") {
+            do {
+                let downloadedData = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decodedAlbum = try! decoder.decode(Album.self, from: downloadedData)
+            } catch {
+                print("Error in decoding the json: \(error)")
+            }
+         
+            do {
+                encodedAlbum = try JSONEncoder().encode(decodedAlbum)
+                let encodedAlbumRep = String(data: encodedAlbum!, encoding: .utf8)
+                print(encodedAlbumRep)
+            } catch {
+             print("Error in encoded the Album: \(error)")
+            }
+            
+        }
     }
 }
