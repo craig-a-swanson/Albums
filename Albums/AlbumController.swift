7daefef109143e8588e331af90febac0b5db95ff
Craig Swanson <cswanson02@me.com>
2020-01-16 16:14:18 -1000

    Implemented getAlbums method.

--- Albums/AlbumController.swift
+++ Albums/AlbumController.swift
@@ -9,43 +9,83 @@
 import Foundation
 
 class AlbumController {
-            var decodedAlbum: Album?
-    var encodedAlbum: Data?
-    func testDecodingExampleAlbum() {
+    
+    // MARK: - Properties
+    var albums: [Album] = []
+    private let baseURL: URL = URL(string: "https://mymovies-1962c.firebaseio.com/")!
+    
+    
+    // MARK: - Read from server
+    func getAlbums(completion: @escaping (Error?) -> () = {_ in }) {
+        let requestURL = baseURL.appendingPathExtension("json")
         
-        if let url = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json") {
-            
-            do {
-                let downloadedData = try Data(contentsOf: url)
-                let decoder = JSONDecoder()
-                decodedAlbum = try! decoder.decode(Album.self, from: downloadedData)
-                print(decodedAlbum)
-            } catch {
-                print("Error in decoding the json: \(error)")
+        URLSession.shared.dataTask(with: requestURL) { data, _, error in
+            guard error == nil else {
+                print("Error fetching albums from server: \(error)")
+                completion(error)
+                return
             }
             
-        }
+            guard let data = data else {
+                print("No data was returned by data task")
+                completion(NSError())
+                return
+            }
+            
+            do {
+                self.albums = Array(try JSONDecoder().decode([String : Album].self, from: data).values)
+                completion(nil)
+            } catch {
+                print("Error decoding albums from json: \(error)")
+                completion(error)
+                return
+            }
+        }.resume()
     }
     
-    func testEncodingExampleAlbum() {
-
-        if let url = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json") {
-            do {
-                let downloadedData = try Data(contentsOf: url)
-                let decoder = JSONDecoder()
-                decodedAlbum = try! decoder.decode(Album.self, from: downloadedData)
-            } catch {
-                print("Error in decoding the json: \(error)")
-            }
-         
-            do {
-                encodedAlbum = try JSONEncoder().encode(decodedAlbum)
-                let encodedAlbumRep = String(data: encodedAlbum!, encoding: .utf8)
-                print(encodedAlbumRep)
-            } catch {
-             print("Error in encoded the Album: \(error)")
-            }
-            
-        }
-    }
+    
+    
 }
+
+
+
+
+
+//            var decodedAlbum: Album?
+//    var encodedAlbum: Data?
+//    func testDecodingExampleAlbum() {
+//
+//        if let url = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json") {
+//            
+//            do {
+//                let downloadedData = try Data(contentsOf: url)
+//                let decoder = JSONDecoder()
+//                decodedAlbum = try! decoder.decode(Album.self, from: downloadedData)
+//                print(decodedAlbum)
+//            } catch {
+//                print("Error in decoding the json: \(error)")
+//            }
+//        }
+//    }
+//
+//    func testEncodingExampleAlbum() {
+//
+//        if let url = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json") {
+//            do {
+//                let downloadedData = try Data(contentsOf: url)
+//                let decoder = JSONDecoder()
+//                decodedAlbum = try! decoder.decode(Album.self, from: downloadedData)
+//            } catch {
+//                print("Error in decoding the json: \(error)")
+//            }
+//
+//            do {
+//                encodedAlbum = try JSONEncoder().encode(decodedAlbum)
+//                let encodedAlbumRep = String(data: encodedAlbum!, encoding: .utf8)
+//                print(encodedAlbumRep)
+//            } catch {
+//             print("Error in encoded the Album: \(error)")
+//            }
+//
+//        }
+//    }
